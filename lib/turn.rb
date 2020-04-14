26970e491e5639d2e7bc79d36d7e2dfe036490d2
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-04-14 11:58:24 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -18,6 +18,10 @@ def position_taken?(board, index)
   end
 end
 
-def valid_move?
-
+def valid_move?(board, index)
+  if position_taken?(board, index) == false && board[index].between?(0,8)
+    true
+  else
+    false
+  end
 end
