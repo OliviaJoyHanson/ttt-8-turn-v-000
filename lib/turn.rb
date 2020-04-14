72a7b0aa348bee0ba47b570994f5266eb9846407
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-04-14 13:00:29 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -37,6 +37,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
+    true
     move(board, index, token = "X")
     display_board(board)
   else
