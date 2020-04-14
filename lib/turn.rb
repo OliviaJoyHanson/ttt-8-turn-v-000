7b2f408c6810e3231c52e8979914c883e7993f9c
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-04-14 12:50:42 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -38,7 +38,8 @@ def turn(board)
   index = input_to_index(input)
   if valid_move?(board, index)
     move(board, index, token = "X")
+    display_board(board)
+  else
     turn(board)
   end
-  display_board(board)
 end
