646f3b4438836c9c91474daeaa9517bdb71807c5
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-04-14 12:39:08 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,7 +36,7 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  status = valid_move(board, index)
+  status = valid_move?(board, index)
   until status == true
     turn(board)
   end
